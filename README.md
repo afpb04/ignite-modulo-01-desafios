360adf38dc094b3f8e1ce8ed6bdc22351db1d96c
Antonio Francisco <afpb04@hotmail.com>
2021-03-20 19:05:50 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# About this repository
+
+
+This repository has three projects developed in module 01 of Ignite by Rocketseat. 
+
+### Todo
+
+This was the first challenge of module 01, the Todo List, with which the concepts of Node.JS were put into practice,
+rules behind an api rest, https methods, main return codes and types of parameters for a requirement and it was even possible to work with middleware.
+
+### Todo middlewares
+
+In this challenge we used the same application and it was possible to work more deeply with middleware in Express. 
+
+### Repositories
+
+In this challenge we took a Node.JS application that was under development, but with some methods missing, the code was corrected and some methods were written so that the application could pass the tests. 
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/)
+- [Yarn](https://classic.yarnpkg.com/) or [npm](https://www.npmjs.com/)
+
+**Clone the project and access the folder**
+
+```bash
+$ git clone https://github.com/afpb04/ignite-modulo-01-desafios.git
+
+cd Modulo-01-Desafios
+```
+**Follow the steps below**
+
+```bash
+#Select the challenge 
+#Inside the chosen challenge folder 
+# Install the dependencies
+$ yarn
+
+# To finish, run the api service
+$ yarn dev
+
+# Well done, project is started!
+
+```
---
+++ Todos-Ignite/src/index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: 'User not found!' });
+  }
+  request.user = user;
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const userNameAlreadExists = users.some((user) => user.username === username);
+
+  if (userNameAlreadExists) {
+    return response.status(400).json({ error: 'Username in use!' });
+  }
+  const user = {
+    name,
+    username,
+    id: uuidv4(),
+    todos: []
+  }
+  users.push(user);
+  return response.status(201).json(user);
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const newTodo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+  user.todos.push(newTodo);
+
+  return response.status(201).json(newTodo);
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+  if (!todo) {
+    return response.status(404).json({ error: 'todo not exist!' });
+  }
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(201).json(todo);
+
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+  if (!todo) {
+    return response.status(404).json({ error: 'todo not exist!' });
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
+
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+  if (!todo) {
+    return response.status(404).json({ error: 'todo not exist!' });
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
+
+});
+
+module.exports = app;
